e69aa13cddecd87c6ae24e53a465e1d28153098f
femalebornhacker <71173230+femalebornhacker@users.noreply.github.com>
2020-09-12 19:52:38 +0000

    Cosmetic changes to follow PEP8
    which are just recommendations on Python code style.
    This is what is call "nitpicking". Code is correct.
    See https://www.python.org/dev/peps/pep-0008/ or
    https://docs.python-guide.org/writing/style/
    In vscode, the "python" extension tells you about these things as you
    type.

--- bch.py
+++ bch.py
@@ -1,24 +1,29 @@
 import hashlib
 import datetime
+
 hashlib.sha256("hola")
 
+
 class Block:
-    def __init__(self, data, previous_hash, timestamp=None): #constructora
+    def __init__(self, data, previous_hash, timestamp=None):  # constructora
         print("soy un bloque...")
         self.timestamp = timestamp or datetime.datetime.utcnow()
         self.data = data
         self.previous_hash = previous_hash
-    def __str__(self): #metodo que es llamado por print by default
+
+    def __str__(self):  # metodo que es llamado por print by default
         print("estoy en el metodo str")
-        return "".join((str(self.timestamp), str(self.data), str(self.previous_hash)))
-#creamos objeto de la clase Block: bloque = Block(0, hola, 0)
-#>>> str(bloque)
-# '0, hola, 0'
-# >>> print(bloque)
-# 0, hola, 0
+        return "".join(
+            (str(self.timestamp), str(self.data), str(self.previous_hash))
+        )
+        # creamos objeto de la clase Block: bloque = Block(0, hola, 0)
+        # >>> str(bloque)
+        # '0, hola, 0'
+        # >>> print(bloque)
+        # 0, hola, 0
+
     def generate_hash(self):
         print("generando hash")
         return hashlib.sha256(str(self).encode()).hexdigest().encode()
         # self.__str__()
         # str(self)
-        
